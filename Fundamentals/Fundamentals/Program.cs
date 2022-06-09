44430d6211fbd9d78be2ef799d5bce66589e8f3d
SrMazas <info.otero.gabriel@gmail.com>
2022-06-09 19:41:17 +0200

    Initial commit

---
+++ Fundamentals/Fundamentals/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solo positivos
+            byte n1 = 255;
+
+            //Admite Negativos
+            sbyte n2 = 126;
+
+            //Admite Negativos
+            int n3;
+
+            //Solo Positivos
+            uint n4;
+
+
+
+            //4 bytes
+            float n5=2f;
+            // 8 bytes
+            double n6 = 1d;
+            //16 bytes
+            decimal n7=1m;
+
+            //char
+            char character = 'A';
+
+            //string
+            string name = "hola";
+
+            long n8;
+
+            DateTime date = DateTime.Now;
+
+            bool siOno= false;
+
+            int numero=new int();
+            
+            //? make nullable
+            int? nTest = null;
+
+            //Solo funciona en local
+            var test = "Hola";
+
+            var persona = new { nombre = "Hola", apellido = "MiApellido" };
+
+            Console.WriteLine(persona.nombre);
+        }
+    }
+}
