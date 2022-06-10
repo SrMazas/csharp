a00aecca83394e9956bc7e58aa0eff5dbd9888f3
SrMazas <info.otero.gabriel@gmail.com>
2022-06-10 13:23:27 +0200

    06/10/2022/01-23PM

---
+++ NavegadorWeb/NavegadorWeb/Program.cs
@@ -0,0 +1,37 @@
+﻿#define TRAZA
+
+using System;
+using System.Windows.Forms;
+
+namespace NavegadorWeb
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Test");
+
+#if PRUEBA
+            Console.Write (“Esto es una prueba”);
+
+#if TRAZA
+            Console.Write(“ con traza”);
+#elif !TRAZA
+
+            Console.Write(“ sin traza”);
+#endif
+#endif
+
+            Console.Write("Test");
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
